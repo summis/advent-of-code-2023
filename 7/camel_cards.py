f76a95b9b6996c8c4c9218593d2f9013af342dfc
summis <>
2023-12-07 22:47:16 +0200

    7/12 second part

--- 7/camel_cards.py
+++ 7/camel_cards.py
@@ -47,8 +47,42 @@ def part1():
 
 
 def part2():
-    pass
+    labels = "A K Q T 9 8 7 6 5 4 3 2 J".split()
+    base_number = len(labels)
+    label_scores = {
+        label: base_number - i for i, label in enumerate(labels)
+    }
 
+    def _score_hand2(hand):
+        counts = defaultdict(int)
+        jokers = 0
+
+        for card in hand:
+            if card != "J":
+                counts[card] += 1
+            else:
+                jokers += 1
+
+        if counts:
+            counts[max(counts.keys(), key=counts.get)] += jokers
+        else:
+            counts["J"] = 5
+
+        primary = sum(math.pow(10, x) for x in counts.values())
+        secondary = tuple(label_scores[card] for card in hand)
+
+        return (primary, secondary)
+
+    hands = _get_hands()
+    ranked_hands = sorted(hands.keys(), key=_score_hand2)
+
+    total = 0
+    rank = 1
+    for hand in ranked_hands:
+        total += hands[hand] * rank
+        rank += 1
+
+    print(total)
 
 def main():
     part1()
