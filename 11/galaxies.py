7200bfd7015f5df637544293a3254c704eb93a9d
summis <>
2023-12-12 18:21:16 +0200

    11/12 second part

--- 11/galaxies.py
+++ 11/galaxies.py
@@ -35,8 +35,35 @@ def part1():
     print(sum(map(lambda x: distance(x[0], x[1]), pairs)))
 
 
+def get_empty_rows(data):
+    return [i for i, row in enumerate(data) if all(x == "." for x in row)]
+
+
+empty_rows = get_empty_rows(data)
+empty_columns = get_empty_rows(transpose(data))
+
+
+def part2():
+    coordinates = []
+    for j, row in enumerate(data):
+        for i, character in enumerate(row):
+            if character == "#":
+                y = j
+                x = i
+                for limit in empty_rows:
+                    if j > limit:
+                        y += 999999
+                for limit in empty_columns:
+                    if i > limit:
+                        x += 999999
+                coordinates.append((y, x))
+
+    print(sum(map(lambda x: distance(x[0], x[1]), combinations(coordinates, 2))))
+
+
 def main():
     part1()
+    part2()
 
 
 if __name__ == "__main__":
