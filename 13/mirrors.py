d398eec122fc0db33a121a944ce3bf8fdb5c7a4d
summis <>
2023-12-13 20:33:49 +0200

    13/12 second part

--- 13/mirrors.py
+++ 13/mirrors.py
@@ -37,8 +37,37 @@ def part1():
     print(100*horizontal_total + vertical_total)
 
 
+from Levenshtein import distance
+
+
+def find_smudged_horizontal_reflection_line(l):
+    for i in range(1, len(l)):
+        if is_smudged_reflection_index(l, i):
+            return i
+    return None
+
+
+def is_smudged_reflection_index(l, n):
+    assert 0 < n < len(l)
+    return sum(distance(x,y) for x, y in zip(reversed(l[:n]), l[n:])) == 1
+
+
 def part2():
-    pass
+    horizontal_total = 0
+    vertical_total = 0
+
+    for x in data:
+        x =  x.splitlines()
+
+        h = find_smudged_horizontal_reflection_line(x)
+        if h:
+            horizontal_total += h
+
+        v = find_smudged_horizontal_reflection_line(transpose(x))
+        if v:
+            vertical_total += v
+
+    print(100*horizontal_total + vertical_total)
 
 
 def main():
