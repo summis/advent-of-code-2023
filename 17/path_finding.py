9d7a1531442cf4024c1a1e43f70c49347279ebbd
summis <>
2023-12-18 12:47:32 +0200

    17/12 second part

--- 17/path_finding.py
+++ 17/path_finding.py
@@ -39,14 +39,13 @@ with open("/home/summis/advent-of-code-2023/17/input") as f:
 directions = (1, -1, 1j, -1j)
 
 
-def djikstra(graph, start):
-    # Does not matter from where arriving to first node as long it is outside the graph
-    initial_state = (start, 1)
+
+def djikstra(graph, start, allowed_consecutive_steps):
+    initial_state = (start, 0)
 
     heat_loss = {}  # Cost of getting to square when coming from certain direction
     todo = PriorityQueue()
-    # Initial state is already in included
-    states = [(node, lenght * step) for node in graph for step in directions for lenght in (1, 2, 3)]
+    states = [(node, lenght * step) for node in graph for step in directions for lenght in allowed_consecutive_steps] + [initial_state]
 
     for _state in states:
         heat_loss[_state] = 0 if _state == initial_state else math.inf
@@ -58,36 +57,41 @@ def djikstra(graph, start):
         min_cost_state = todo.get().state
         node, route = min_cost_state
 
-        for step in directions:
-            if cmath.phase(-route) == cmath.phase(step): continue
+        for (step, repeats) in ((direction , length) for length in allowed_consecutive_steps for direction in directions):
+            if route and cmath.phase(-route) == cmath.phase(step): continue
 
-            neighbor = node + step
+            neighbor = node + repeats * step
 
             if neighbor not in graph: continue
 
             if cmath.phase(route) == cmath.phase(step):
-                route_to_neighbor = route + step  
+                route_to_neighbor = route + repeats * step 
             else:
-                route_to_neighbor = step
+                route_to_neighbor = repeats * step 
 
-            if abs(route_to_neighbor) > 3: continue
+            if abs(route_to_neighbor) > allowed_consecutive_steps[-1]: continue
 
-            loss = heat_loss[min_cost_state] + graph[neighbor]
+            loss = heat_loss[min_cost_state] + sum(graph[node + i * step] for i in range(1, repeats+1))
             neighbor_state = (neighbor, route_to_neighbor)
+
             if loss < heat_loss[neighbor_state]:
                 heat_loss[neighbor_state] = loss
                 previous[neighbor_state] = min_cost_state
                 todo.put(HeatLossPrioritizedState(loss, neighbor_state))
 
-    return previous
+    return previous, heat_loss
 
 
 def part1():
     start = complex(0, 0)
     end = complex(140, 140)
-    paths = djikstra(graph, start)
+    allowed_consecutive_tiles = (1, 2, 3)
+    paths, losses = djikstra(graph, start, allowed_consecutive_tiles)
 
     ends = [(_end, d) for (_end, d) in paths if _end == end]
+
+    heat_losses = [losses[x] for x in ends]
+    best_path = min(range(len(heat_losses)), key=heat_losses.__getitem__)
 
     parsed_path = []
     for x in ends:
@@ -98,8 +102,42 @@ def part1():
             x = paths[x]
         parsed_path.append(full_path)
 
-    heat_losses = [sum(graph[point] for point in l) for l in parsed_path]
+    visualization = ""
+    for j in range(int(end.imag)+1):
+        for i in range(int(end.real)+1):
+            if complex(i, j) in parsed_path[best_path]:
+                visualization += "#"
+            else:
+                visualization += "."
+        visualization += "\n"
+
+    print(heat_losses[best_path])
+    print(visualization)
+
+
+
+# same as part 1 but neighbors are 4 - 10 steps away
+def part2():
+    start = complex(0, 0)
+    end = complex(140, 140)
+    allowed_consecutive_tiles = (4, 5, 6, 7, 8, 9, 10)
+    paths, losses = djikstra(graph, start, allowed_consecutive_tiles)
+
+    ends = [(_end, d) for (_end, d) in paths if _end == end]
+
+    heat_losses = [losses[x] for x in ends]
+
+    print(heat_losses)
     best_path = min(range(len(heat_losses)), key=heat_losses.__getitem__)
+
+    parsed_path = []
+    for x in ends:
+        full_path = []
+        while x in paths:
+            s, _ = x
+            full_path = [s] + full_path
+            x = paths[x]
+        parsed_path.append(full_path)
 
     visualization = ""
     for j in range(int(end.imag)+1):
@@ -115,3 +153,4 @@ def part1():
 
 
 part1()
+part2()
