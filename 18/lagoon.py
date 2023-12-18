2d6ea9bfd62cb9ec908711dfc8b3b26b407356f0
summis <>
2023-12-18 21:42:25 +0200

    18/12 second part

--- 18/lagoon.py
+++ 18/lagoon.py
@@ -1,8 +1,3 @@
-import sys
-
-sys.setrecursionlimit(150000)
-
-
 
 with open("input") as f:
     data = f.read().splitlines()
@@ -15,14 +10,15 @@ lookup = {
     "R": 1
 }
 
+
 def parse_line(data):
     prev = complex(0, 0)
-    result = set([prev])
+    result = [prev]
 
     for [direction, _repeats, _] in (x.split() for x in data):
         for _ in range(int(_repeats)):
             new = prev + lookup[direction]
-            result.add(new)
+            result.append(new)
             prev = new
         
     return result
@@ -44,8 +40,6 @@ def flood_and_fill(start, candidates):
     return ret
 
 
-# 48653 is too high
-# 48652 is correct, using set when parsing cycle helped :)
 def part1():
     path = parse_line(data)
 
@@ -74,7 +68,46 @@ def part1():
     
     # Insight from data: there are only two regions -> only one "inside" and one "outside"
     inner_region = [x for x in regions if complex(i_min-1, j_min -1) not in x][0]
-    print(len(inner_region) + len(path))
+    print(len(inner_region) + len(set(path)))
+
+
+
+def parse_line_2(data):
+    prev = complex(0, 0)
+    result = [prev]
+
+    for [_, _, color] in (x.split() for x in data):
+        hexa = color[2:-1]
+        length = int("0x"+hexa[:-1], 0)
+        direction = {
+            "0": "R",
+            "1": "D",
+            "2": "L",
+            "3": "U"
+        }[hexa[-1]]
+
+        result.append(result[-1] + length * lookup[direction])
+
+    return result
+
+
+def pairwise(l):
+    return zip(l, l[1:])
+
+
+def calculate_area(path):
+    # https://en.wikipedia.org/wiki/Shoelace_formula
+    polygon_area = abs(sum(z1.real * z2.imag - z1.imag * z2.real for z1, z2 in pairwise(path)) / 2)
+    perimeter_area = sum(abs(z2 - z1) for z1, z2 in pairwise(path))
+    # https://en.wikipedia.org/wiki/Pick%27s_theorem
+    # here returning "points inside polygon + points in perimeter"
+    return polygon_area + perimeter_area / 2 + 1
+
+
+def part2():
+    path = parse_line_2(data)
+    print(calculate_area(path))
 
 
 part1()
+part2()
