12114ec7110f692aa0e6d2e6be0ec5bd7ff56e93
summis <>
2023-12-20 00:24:48 +0200

    19/12 second part

--- 19/workflows.py
+++ 19/workflows.py
@@ -62,36 +62,67 @@ def parse_flows_2(rules):
     return parsed
 
 
-def apply_rule(e, rules):
-    rule = rules[0]
-
-    if len(rules) == 1: return rule
-    
-    if "<" in rule:
-        [c, value, target] = rule.replace(":", "<").split("<")
-        if e[c] < int(value):
-            return target
-        return apply_rule(e, rules[1:])
-
-    if ">" in rule:
-        [c, value, target] = rule.replace(":", ">").split(">")
-        if e[c] > int(value):
-            return target
-        return apply_rule(e, rules[1:])
-
-
 def part2():
-    parts = parse_inputs(inputs)
+    todos = [("in", {"m": (1, 4001), "a": (1, 4001), "s": (1, 4001), "x": (1, 4001)})]
     rules = parse_flows_2(flows)
     handled = {"A": [], "R": []}
 
-    for part in parts:
-        node = "in"
-        while node not in ("A", "R"):
-            node = apply_rule(part, rules[node])
-        handled[node].append(part)
-    
-    return sum(sum(x.values()) for x in handled["A"])
-  
+    def apply_rule(e, rules):
+        rule = rules[0]
+
+        if len(rules) == 1:
+            todos.append((rule, e))
+
+        if "<" in rule:
+            [c, value, target] = rule.replace(":", "<").split("<")
+
+            r1, r2 = e[c]
+
+            true_range = (r1, min(r2, int(value)))
+            if true_range[0] < true_range[1]:
+                e1 = e.copy()
+                e1[c] = true_range
+                todos.append((target, e1))
+
+            false_range = (max(r1, int(value)), r2)
+            if false_range[0] < false_range[1]:
+                e2 = e.copy()
+                e2[c] = false_range
+                apply_rule(e2, rules[1:])
+
+        if ">" in rule:
+            [c, value, target] = rule.replace(":", ">").split(">")
+
+            r1, r2 = e[c]
+
+            true_range = (max(r1, int(value) + 1), r2)
+            if true_range[0] < true_range[1]:
+                e1 = e.copy()
+                e1[c] = true_range
+                todos.append((target, e1))
+
+            false_range = (r1, min(r2, int(value) + 1))
+            if false_range[0] < false_range[1]:
+                e2 = e.copy()
+                e2[c] = false_range
+                apply_rule(e2, rules[1:])
+
+    while todos:
+        node, e = todos.pop()
+
+        if node in ("A", "R"):
+            handled[node].append(e)
+            continue
+
+        apply_rule(e, rules[node])
+
+    return sum(
+        (x["a"][1] - x["a"][0])
+        * (x["s"][1] - x["s"][0])
+        * (x["x"][1] - x["x"][0])
+        * (x["m"][1] - x["m"][0])
+        for x in handled["A"]
+    )
+
 
 print(part1(), part2())
